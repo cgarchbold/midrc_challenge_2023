2c8a7addf91599e417e421da67e017347005e0f6
atik-ahamed <atikahamedutso@gmail.com>
2023-06-12 12:45:46 -0400

    Completed training ResNet-50  w/ Cropping augmentation 100 epochs

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 config={
     'wandb': True,
-    'experiment_name': 'ResNet-50, Augment without cropping (Epoch=100)', # Must be an acceptable directory name!
+    'experiment_name': 'ResNet-50, Augment with cropping (Epoch=100)', # Must be an acceptable directory name!
     'model':'ResNet-50',
     'imgnet_pretrained':True,
     'epochs':100,
     'augment' : True, # True: Applies augmentations
-    'cropping_augmentation':False, # True: Applies cropping augmentations, False: w/o cropping augmentations
+    'cropping_augmentation':True, # True: Applies cropping augmentations, False: w/o cropping augmentations
     'batch_size': 16,
     'learning_rate': 0.001,
     'root_dir': '../data/resized_224X224',
