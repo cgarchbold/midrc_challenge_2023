6ad8d7ef119732ba663faa7075e9967a0aa56880
cgarchbold <cgarchbold@gmail.com>
2023-06-15 09:26:35 -0400

    512px Experiment

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 config={
     'wandb': True,
-    'experiment_name': 'ResNet-50, Only Rotation without cropping (Epoch=100), cos scheduler + adamw', # Must be an acceptable directory name!
+    'experiment_name': '512px ResNet-50, Augment(Random Rotation(20), HorizF(0.1)) without cropping (Epoch=100), cos scheduler + adamw', # Must be an acceptable directory name!
     'model':'ResNet-50',
     'imgnet_pretrained':True,
     'epochs':100,
     'augment' : True, # True: Applies augmentations
     'cropping_augmentation':False, # True: Applies cropping augmentations, False: w/o cropping augmentations
-    'batch_size': 16,
+    'batch_size': 8,
     'learning_rate': 0.001,
     'scheduler':'cos',
     'scheduler_warmup':1,
-    'optim': 'adamw', 
-    'root_dir': '../data/resized_224X224',
+    'optim': 'adamw',
+    'root_dir': '../data/resized_512X512',
     'annotations_path': 'MIDRC mRALE Mastermind Training Annotations_2079_20230428.csv'
 }
